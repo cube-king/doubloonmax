5bb7acf10026f19f69863f8a9cbeb30dc85a5d1b
cube-king <100381926+cube-king@users.noreply.github.com>
2025-01-06 21:24:44 +0000

    cosmetic changes

--- index.js
+++ index.js
@@ -3,58 +3,66 @@ jQuery(function(){
     var total = 0;
 
     $('#btn').on("click", function () {
-        if (item < 13) {
-            var hrs = $('.hrsinput').val();
-            var dbls = $('.dblsinput').val();
-            var name = $('.nameinput').val();
-            var goal = $('.goalinput').val();
-            var totaldblhr = 0;
-            var average = 0;
+    
+        var hrs = $('.hrsinput').val();
+        var dbls = $('.dblsinput').val();
+        var name = $('.nameinput').val();
+        var goal = $('.goalinput').val();
+        var totaldblhr = 0;
+        var average = 0;
 
-            item += 1;
-            // Increment the total of doublons
-            total += Number(dbls);
+        item += 1;
+        // Increment the total of doublons
+        total += Number(dbls);
 
-            // Append the new list item
-            $('.bloonlist').append(
-                '<li class="listitem" id="item' + Number(item) +
-                '" data-dblsprhr="' + Number(dbls) +
-                '" data-position="' + (Number(dbls) / Number(hrs)).toFixed(2) +
-                '">' + (Number(dbls) / Number(hrs)).toFixed(2) + 
-                " doubloons per hour on " + name + '</li>'
-            );
+        // Append the new list item
+        $('.bloonlist').append(
+            '<li class="listitem" id="item' + Number(item) +
+            '" data-dblsprhr="' + Number(dbls) +
+            '" data-position="' + (Number(dbls) / Number(hrs)).toFixed(2) +
+            '">' + (Number(dbls) / Number(hrs)).toFixed(2) + 
+            " doubloons per hour on " + name + '</li>'
+        );
 
-            // Sort the list items
-            $(".bloonlist li").sort(sort_li).appendTo('.bloonlist');
+        // Sort the list items
+        $(".bloonlist li").sort(sort_li).appendTo('.bloonlist');
 
-            // Recalc totaldblhr by iterating through and adding all `data-position` attributes
-            $(".bloonlist li").each(function() {
-                totaldblhr += Number($(this).data('position'));
-            });
+        // Recalc totaldblhr by iterating through and adding all `data-position` attributes
+        $(".bloonlist li").each(function() {
+            totaldblhr += Number($(this).data('position'));
+        });
 
-            console.log("Total dbls per hr:", totaldblhr);
-            console.log("Total dbls:", total);
+        console.log("Total dbls per hr:", totaldblhr);
+        console.log("Total dbls:", total);
 
-            // Calculate and log the average
-            if (item > 0) {
-                average = totaldblhr / item;
-                console.log("Average:", average);
-            }
+        // Calculate and log the average
+        if (item > 0) {
+            average = totaldblhr / item;
+            console.log("Average:", average);
+        }
 
-            // Sorting function
-            function sort_li(a, b) {
-                return ($(b).data('position')) < ($(a).data('position')) ? -1 : 1;
-            }
-
-            if (goal <= total && !isNaN(goal) && !isNaN(total) ) {
-                $('.rain').css('opacity', '1');
+        // Sorting function
+        function sort_li(a, b) {
+            return ($(b).data('position')) < ($(a).data('position')) ? -1 : 1;
+        }
+        if (goal <= total && !(goal).isNaN && !(total).isNan && goal > 0) {
+            $('.rain').css('opacity', '1');
+        }
+        else {
+            $('.rain').css('opacity', '0');
+        }
+        $(".totaldbls").text("Total doubloons: "+total.toFixed(2))
+        $(".avgdblsperhr").text("Average doubloons per hour: "+average.toFixed(2))
+        if (goal > 0 && !goal.isNaN) {
+            if (Boolean((goal - total)/average > 0)) {
+                $(".hrsuntilgoal").text("Hours until goal: "+((goal - total)/average).toFixed(2))
             }
             else {
-                $('.rain').css('opacity', '0');
+                $(".hrsuntilgoal").text("Goal completed!")
             }
-            $(".totaldbls").text("Total doubloons: "+total.toFixed(2))
-            $(".avgdblsperhr").text("Average doubloons per hour: "+average.toFixed(2))
-            $(".hrsuntilgoal").text("Hours until goal: "+((goal - total)/average).toFixed(2))
+         }
+        else {
+            $(".hrsuntilgoal").text("Hours until goal (placeholder)!")
         }
     });
 
@@ -65,7 +73,7 @@ jQuery(function(){
         $('#item' + item).remove();
         item -= 1;
         console.log(total);
-        if (goal <= total && !isNaN(goal) && !isNaN(total)) {
+        if (goal <= total && !(goal).isNaN && !(total).isNan && goal > 0) {
             $('.rain').css('opacity', '1');
         }
         else {
@@ -73,7 +81,17 @@ jQuery(function(){
         }
         $(".totaldbls").text("Total doubloons: "+total.toFixed(2))
         $(".avgdblsperhr").text("Average doubloons per hour: "+average.toFixed(2))
-        $(".hrsuntilgoal").text("Hours until goal: "+((goal - total)/average).toFixed(2))
+        if (goal > 0 && !goal.isNaN) {
+            if (Boolean((goal - total)/average > 0)) {
+                $(".hrsuntilgoal").text("Hours until goal: "+((goal - total)/average).toFixed(2))
+            }
+            else {
+                $(".hrsuntilgoal").text("Goal completed!")
+            }
+        }
+        else {
+            $(".hrsuntilgoal").text("Hours until goal (placeholder)!")
+        }
     });
 
     $('.ship').on("click", function () {
